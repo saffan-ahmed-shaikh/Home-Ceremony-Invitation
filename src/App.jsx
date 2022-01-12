cd9714daaa07540af3342cc1a0b62cae6de382c7
saffan-ahmed-shaikh <saffan20102@gmail.com>
2022-01-12 22:34:43 +0530

    invitation

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="invitationBgFrame">
+        <div className="quotesSection">
+          <h3>
+            A House is made of bricks & beams, <br />A Home is made of hopes &
+            dreams!
+          </h3>
+          <h4>MR. KAMALUDEEN FAMILY</h4>
+          <p>
+            cordinally invite you & your family <br />
+            on the auspicious ocassion of
+          </p>
+          <h1>
+            HOUSEWARMING <br /> CEREMONY
+          </h1>
+          <h5>of Our New House</h5>
+          <h2 className="inviteDate">
+            ON SUNDAY, 16<sup>th</sup> JANUARY 2022
+          </h2>
+          <h2 className="inviteTime">at 11am</h2>
+          <address>
+            Sector:- 8, Vijayarajan Home, Room No. 404, 4<sup>th</sup> Floor,
+            Kharkopar, Navi Mumbai-401208.
+          </address>
+        </div>
+      </div>
+      <div className="whiteSpace"></div>
+    </div>
+  );
+}
+
+export default App;
